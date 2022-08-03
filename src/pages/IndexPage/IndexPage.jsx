8db1a75dd52f5f8377780611d8a05f0db36d64b6
Vasiliy <baz_raider@mail.ru>
2022-08-03 12:28:32 +0700

    setting leaflet, map showing, markers and popups working

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import './IndexPage.scss';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import pointsData from '../../data/points.json'
 
 export default function IndexPage() {
   return (
-    <div id="map">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
+    <MapContainer
+      center={[55.752185526060266, 37.617590770030965]}
+      zoom={10}
+      style={{ width: '100vw', height: '100vh' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {pointsData.features.map(point => (
+        <Marker
+          key={point.properties.POINT_ID}
+          position={[
+            point.geometry.coordinates[0],
+            point.geometry.coordinates[1]
+          ]}
+        >
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            <h2>{point.properties.NAME}</h2>
+            <p>{point.properties.DESCRIPTIO}</p>
           </Popup>
         </Marker>
-      </MapContainer>
-    </div>
+      ))}
+    </MapContainer>
   )
 }
